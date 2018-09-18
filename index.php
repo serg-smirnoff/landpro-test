933f8f4a12da37aabaa4dc41eea4850857437b12
root <info@serg-smirnoff.com>
2018-09-18 14:04:21 +0300

    Изменил подключение пространства имен‏

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once ('libs/amo.php');
 require_once ('vendor/autoload.php');
 require_once ('libs/googlesheets.php');
 
-use AMO;
-use GOOGLESHEETS;
+use AMO as AMO;
+use GOOGLESHEETS as GOOGLESHEETS;
 
 $amo = new \AMO\Amo();
 
